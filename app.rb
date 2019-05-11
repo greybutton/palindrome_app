2cdbb808841c59cc37f01527e5d8f98fd8a7aa47
greybutton <greybutton@yandex.com>
2019-05-11 17:54:40 +0800

    done 10.5 (add palindrome detector)

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'greybutton_palindrome'
 
 get '/' do
   @title = 'Home'
@@ -13,4 +14,10 @@ end
 get '/palindrome' do
   @title = 'Palindrome Detector'
   erb :palindrome
+end
+
+post '/check' do
+  @title = 'Result'
+  @phrase = params[:phrase]
+  erb :result
 end
